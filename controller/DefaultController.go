da8a52d9e9febab74f40e69d8a89735882bd1bfe
zhangchunhui <chunhui2001@gmail.com>
2022-08-22 00:25:50 +0800

    a
    a

--- controller/DefaultController.go
+++ controller/DefaultController.go
@@ -6,29 +6,13 @@ import (
 	"github.com/chunhui2001/go-starter/config"
 	"github.com/chunhui2001/go-starter/mycache"
 	"github.com/chunhui2001/go-starter/utils"
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func IndexRouter(c *gin.Context) {
-
-	APP_COOKIE := config.CookieSetting
-	var yourRoomId string
-
-	if APP_COOKIE.Enable {
-		session := sessions.Default(c)
-		if session.Get("yourRoomId") == nil {
-			session.Set("yourRoomId", utils.ShortId())
-			session.Save()
-		}
-		yourRoomId = utils.ToString(session.Get("yourRoomId"))
-	} else {
-		yourRoomId = mycache.ShortIdPut(utils.ShortId())
-	}
-
 	c.HTML(http.StatusOK, "index", gin.H{
 		"wssEndpoint": config.WssSetting.Wss(),
-		"yourRoomId":  yourRoomId,
+		"yourRoomId":  mycache.ShortIdPut(utils.ShortId()),
 		"content":     "This is an Home page...",
 	})
 }
