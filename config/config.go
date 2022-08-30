a1ff183d93ef52ef2f89bd0570635d3e907447ec
zhangchunhui <chunhui2001@gmail.com>
2022-08-30 18:55:52 +0800

    a
    a

--- config/config.go
+++ config/config.go
@@ -275,7 +275,7 @@ func loadAppSettings(v1 *viper.Viper, filename string) {
 		os.Exit(3)
 		return
 	} else {
-		log.Println("AppSetting: TimeZone=" + AppSetting.TimeZone + " Env=" + AppSetting.Env + ", AppName=" + AppSetting.AppName + ", AppPort=" + AppSetting.AppPort + ", appRoot=" + utils.RootDir())
+		log.Println("AppSetting: TimeZone=" + AppSetting.TimeZone + ", Env=" + AppSetting.Env + ", AppName=" + AppSetting.AppName + ", AppPort=" + AppSetting.AppPort + ", appRoot=" + utils.RootDir())
 	}
 }
 
