8ccce8415583dd447dab7c2074f48d48be9240cd
zhangchunhui <chunhui2001@gmail.com>
2022-11-10 15:59:23 +0800

    a
    a

--- core/goes/openes.go
+++ core/goes/openes.go
@@ -188,7 +188,7 @@ func PutMapping(mappingName string, jsonTemplate string) bool {
 	requestUrl := fmt.Sprintf(`%s/_template/%s`, serverUri, mappingName)
 
 	httpResult := ghttp.SendRequest(
-		ghttp.PUT(requestUrl, jsonTemplate),
+		ghttp.PUT(requestUrl, jsonTemplate).AddHeader("Content-Type", "application/json"),
 	)
 
 	return httpResult.Success()
